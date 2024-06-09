5ae48ea8c3455cb985d561ba9036b64b95dec404
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2024-06-09 10:34:14 +0200

    First server confing

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-var functions = require('./functions');
-functions.sayHello();
-functions.add(3, 2); 
+const http = require('http');
+const handler = (request, response) => {
+    console.log("sample message");
+}
+const server = http.createServer(handler);
+const port = 3000;
+server.listen(port, () => {
+    console.log("serwer działa na porcie", port);
+})
--- functions.js
+++
@@ -1,9 +0,0 @@
-const sayHello = () => { console.log("Hello World") }
-const add = (a, b) => {
-    console.log(a + b)
-}
-module.exports = {
-    sayHello,
-    add
-}
-console.log("Everything is loaded correctly")
