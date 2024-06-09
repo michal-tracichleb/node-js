082ad8eac6f52e5561cc65c2c2a9c5701374c413
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2024-06-09 11:16:17 +0200

    Add page title

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ app.use('/assets', express.static(path.join(__dirname, "./assets")));
 app.use('/js', express.static(path.join(__dirname, "./js")));
 app.use('/js', express.static(__dirname + './../public/js'));
 
+
 //ustawienie, ze moja aplikacja musi korzystac z silnika hbs 
 app.set("view engine", 'hbs')
 //gdy uzytkownik wchodzi na stronę  
 app.get('/', function (req, res) {
-    res.render('index')
+    res.render('index', {
+        pageTitle: "Michał Tracichleb"
+    })
 })
 app.get('/about', function (req, res) {
     res.send("My site")
