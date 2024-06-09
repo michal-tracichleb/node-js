db9c7adc79d545c7acc048814e7e379ac486c6e2
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2024-06-09 10:36:52 +0200

    Error handling

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const handler = (request, response) => {
 }
 const server = http.createServer(handler);
 const port = 3000;
-server.listen(port, () => {
-    console.log("serwer działa na porcie", port);
+server.listen(port, (err) => {
+    if (err) {
+        return console.log("coś poszło nie tak...:", err)
+    }
+    console.log("serwer działa na porcie", port)
 })
