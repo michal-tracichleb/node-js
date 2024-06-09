33042731b0ef9a3181f735b31ed138af66a645c6
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2024-06-09 10:42:02 +0200

    Add node express - https

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const http = require('http');
-const handler = (request, response) => {
-    console.log("sample message");
-    response.end("Hello World");
-}
-const server = http.createServer(handler);
+const express = require("express"); //nie korzystam już z http!!
 const port = 3000;
-server.listen(port, (err) => {
+const app = express();
+//gdy uzytkownik wchodzi na stronę główną
+app.get('/', function (req, res) {
+    res.send("Hello World");
+})
+//gdy uzytkownik wchodzi na stronę o nas
+app.get('/about', function (req, res) {
+    res.send("My site")
+})
+app.listen(port, (err) => {
     if (err) {
         return console.log("coś poszło nie tak...:", err)
     }
     console.log("serwer działa na porcie", port)
-})
+}) 
